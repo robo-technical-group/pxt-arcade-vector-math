1abd555ccedf6d2cd0516281434c64f7e2902725
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2019-03-17 11:35:35 -0400

    Initial commit.

--- test.ts
+++ test.ts
@@ -1,1 +1,28 @@
-// tests go here; this will not be compiled when this package is used as a library
+const numRuns: number = 5
+let currRun: number = 1
+let beginSprite: Sprite = sprites.create(sprites.castle.skellyFront, 1)
+let endSprite: Sprite = sprites.create(sprites.food.smallBurger, 2)
+resetBoard()
+
+sprites.onOverlap(1, 2, function (sprite: Sprite, otherSprite: Sprite) {
+    sprite.say(":)")
+    sprite.vx = 0
+    sprite.vy = 0
+    loops.pause(1000)
+    currRun++
+    if (currRun <= numRuns) {
+        resetBoard()
+    }   // if (currRun <= numRuns)
+})  // sprites.onOverlap(1, 2, ...)
+
+function resetBoard() {
+    beginSprite.say("")
+    beginSprite.x = Math.randomRange(0, screen.width)
+    beginSprite.y = Math.randomRange(0, screen.height)
+    endSprite.x = Math.randomRange(0, screen.width)
+    endSprite.y = Math.randomRange(0, screen.height)
+    let vector: vectorMath.Vector =
+        vectorMath.createVectorFromSprites(beginSprite, endSprite)
+    beginSprite.vx = vector.x
+    beginSprite.vy = vector.y
+}   // resetBoard()
